40fe9d374670d92377ee4290d5b9ae44155b2412
poorveshmuthuraman <m.poorvesh@gmail.com>
2024-07-27 14:34:14 +0530

    complete footer

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div className='sidebar'>
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>The Brutal Marshal Landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat sint id soluta culpa voluptatibus similique ratione veritatis in, corporis dolorem magnam accusantium nostrum sunt quis nisi suscipit tempora modi eligendi.</p>
+        </div>
+      </div>
+    </div>
+
+  )
+}
