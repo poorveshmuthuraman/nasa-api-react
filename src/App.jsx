f94f610b8d311f1a868774ac5678af9daeb9536a
poorveshmuthuraman <m.poorvesh@gmail.com>
2024-08-02 20:46:20 +0530

    api call does not work

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@
 import Main from './components/Main'
 import SideBar from './components/SideBar'
 import Footer from './components/Footer'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
   const [showModal, setShowModel] = useState(false)
+  
+  useEffect(() =>{
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      const url = 'GET https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+      try {
+        const res = await fetch(url)
+        console.log(res)
+        const data = await res.json()
+        console.log('DATA: \n\n', data)
+      } catch(err) {
+        console.log(err.message)
+      }
+    }
+
+    fetchAPIData()
+  }, [])
   
   function handleToggleModal() {
     setShowModel(!showModal)
